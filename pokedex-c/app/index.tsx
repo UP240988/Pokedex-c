731547071b7f638449a258010bd367c2ea1ec81f
Jorge Emilio <up240988@alumnos.upa.edu.mx>
2026-03-02 15:30:22 -0600

    Add pokemonfetch

--- pokedex-c/app/index.tsx
+++ pokedex-c/app/index.tsx
@@ -1,14 +1,40 @@
+import { useEffect} from "react";
 import { Text, View } from "react-native";
 
 export default function Index() {
+
+  useEffect(() => {
+    console.log("Entre en pantalla");
+    getPokemons();
+  }, []);
+
+
+
+  const getPokemons = async() => {
+    try{
+
+    
+  const URL = "https://pokeapi.co/api/v2/pokemon?limit=151";
+  const response = await fetch (URL, {
+  method: "GET",  
+
+  });
+  if (response.ok){
+    console.log("Request ok");
+  } else {
+    console.log("Bard Request");
+  }
+} catch(error){
+  console.log("Ocurrio un error") 
+}
+
+  };
+
+
+  
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View>
       <Text>Edit app/index.tsx to edit this screen.</Text>
     </View>
   );
