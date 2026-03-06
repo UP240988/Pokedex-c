ecb51bf18e764f2e0da3f090f292e51f6037190e
Jorge Emilio <up240988@alumnos.upa.edu.mx>
2026-03-06 15:12:04 -0600

    implementamos el Routing

--- pokedex-c/app/index.tsx
+++ pokedex-c/app/index.tsx
@@ -1,11 +1,9 @@
-import { use, useEffect, useState } from "react";
-import { Text, View } from "react-native";
-import { get } from "react-native/Libraries/TurboModule/TurboModuleRegistry"
+import { useEffect, useState } from "react";
+import { ScrollView } from "react-native";
 import PokemonCard from "../components/PokemonCard";
 
 export default function Index() {
-
-const [results, setResults] = useState<any[]>([]);
+  const [results, setResults] = useState<any[]>([]);
   useEffect(() => {
     console.log("Entre en pantalla");
     getPokemons();
@@ -17,30 +15,31 @@ const [results, setResults] = useState<any[]>([]);
 
   const getPokemons = async () => {
     try {
-      const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0"
-    const response = await fetch(URL, {
-      method: "GET",
-    });
-    if (response.ok) {
-      console.log(response);
-      const data = await response.json();
-      setResults(data.results);
-    } else {
-      console.log("Bad Request");
+      const URL = "https://pokeapi.co/api/v2/pokemon?limit=100000&offset=0";
+      const response = await fetch(URL, {
+        method: "GET",
+      });
+      if (response.ok) {
+        console.log(response);
+        const data = await response.json();
+        setResults(data.results);
+      } else {
+        console.log("Bad Request");
+      }
+    } catch (error) {
+      console.log("Error fetching pokemons:", error);
     }
-  } catch (error) {
-    console.log("Error fetching pokemons:", error);
-  }
-    }
+  };
 
   return (
-    <View>
+    <ScrollView>
       {results.map((item) => (
-        <PokemonCard 
-        key={item.name} 
-        name={item.name} 
-        url={item.url}></PokemonCard>
+        <PokemonCard
+          key={item.name}
+          name={item.name}
+          url={item.url}
+        ></PokemonCard>
       ))}
-    </View>
+    </ScrollView>
   );
 }
--- pokedex-c/components/PokemonCard.tsx
+++ pokedex-c/components/PokemonCard.tsx
@@ -1,23 +1,41 @@
-import {Image, View, Text } from "react-native";
-import React from "react";
+import React from 'react';
+import { View, Image, Text, Pressable,  StyleSheet } from 'react-native';
+import {router} from "expo-router";
 
 interface PokemonCardProps {
-    name: string;
-    url: string;
+  name: string;
+  url: string;
 }
-
 
 export default function PokemonCard(props: PokemonCardProps) {
     const id = props.url.split("/").filter(Boolean).at(-1);
-    const pokemonImageURL = 
-    `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
-    return (
-        <View>
-            <Image 
-            source={{ uri: pokemonImageURL }}
-            style={{ width: 100, height: 100 }}
-            ></Image>
-            <Text>{props.name}</Text>
-        </View>
-    );
+    console.log(id);
+  const pokemonImageURL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
+
+  return (
+    <Pressable 
+    onPress={() => router.push console.log("/new-screen")}
+    style={({pressed}) =>[
+      style.pressableSheet,
+      pressed && {
+        opacity:0.5   }
+    ] }
+    >
+      <View>
+        <Image
+          source={{ uri: pokemonImageURL }}
+          style={{ width: 100, height: 100 }}
+        ></Image>
+        <Text>{props.name}</Text>
+        <Text>{id}</Text>
+      </View>
+    </Pressable>
+  );
 }
+const style = StyleSheet.create({
+  pressableSheet: {
+    borderWidth:1,
+    alignItems: "center",
+    backgroundColor: "cyan",
+  },
+})
