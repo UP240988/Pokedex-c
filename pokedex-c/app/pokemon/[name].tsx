dde71ee4d0ac1d96f701cf65a0e74237c89cc8ed
Jorge Emilio <up240988@alumnos.upa.edu.mx>
2026-03-06 15:26:27 -0600

    Implementamos el Routing

--- pokedex-c/app/pokemon/[name].tsx
+++ pokedex-c/app/pokemon/[name].tsx
@@ -1,11 +1,14 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text } from "react-native";
+import { useLocalSearchParams } from "expo-router";
 
 export default function PokemonScreen() {
-    const params = useLocalParams();
+
+  const { id, name } = useLocalSearchParams();
+
   return (
     <View>
-      <Text>{params.name}</Text>
+      <Text>Nombre: {name}</Text>
+      <Text>ID: {id}</Text>
     </View>
-  )
+  );
 }
--- pokedex-c/components/PokemonCard.tsx
+++ pokedex-c/components/PokemonCard.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
-import { View, Image, Text, Pressable,  StyleSheet } from 'react-native';
-import {router} from "expo-router";
+import { View, Image, Text, Pressable, StyleSheet } from 'react-native';
+import { router } from "expo-router";
 
 interface PokemonCardProps {
   name: string;
@@ -8,34 +8,41 @@ interface PokemonCardProps {
 }
 
 export default function PokemonCard(props: PokemonCardProps) {
-    const id = props.url.split("/").filter(Boolean).at(-1);
-    console.log(id);
+  const id = props.url.split("/").filter(Boolean).at(-1);
+  console.log(id);
+
   const pokemonImageURL = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${id}.png`;
 
   return (
-    <Pressable 
-    onPress={() => router.push console.log("/new-screen")}
-    style={({pressed}) =>[
-      style.pressableSheet,
-      pressed && {
-        opacity:0.5   }
-    ] }
+    <Pressable
+      onPress={() => {
+        console.log("Navigating...");
+        router.push({
+          pathname: "/new-screen",
+          params: { id, name: props.name }
+        });
+      }}
+      style={({ pressed }) => [
+        styles.pressableSheet,
+        pressed && { opacity: 0.5 }
+      ]}
     >
       <View>
         <Image
           source={{ uri: pokemonImageURL }}
           style={{ width: 100, height: 100 }}
-        ></Image>
+        />
         <Text>{props.name}</Text>
         <Text>{id}</Text>
       </View>
     </Pressable>
   );
 }
-const style = StyleSheet.create({
+
+const styles = StyleSheet.create({
   pressableSheet: {
-    borderWidth:1,
+    borderWidth: 1,
     alignItems: "center",
     backgroundColor: "cyan",
   },
-})
+});
